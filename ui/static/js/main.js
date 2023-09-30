06671993a396b57cce3690100e6c7345e4732b38
Nicolas Guerin <disconico@outlook.com>
2023-09-30 09:58:51 +0200

    add style

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,8 +1,8 @@
-var navLinks = document.querySelectorAll("nav a");
-for (var i = 0; i < navLinks.length; i++) {
-	var link = navLinks[i]
-	if (link.getAttribute('href') == window.location.pathname) {
-		link.classList.add("live");
-		break;
-	}
+const navLinks = document.querySelectorAll("nav a");
+for (let i = 0; i < navLinks.length; i++) {
+    const link = navLinks[i];
+    if (link.getAttribute('href') === window.location.pathname) {
+        link.classList.add("live");
+        break;
+    }
 }
