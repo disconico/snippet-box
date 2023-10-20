0e5cb96086de18128ffb0ad83a8e87f93ac1022f
Nicolas Guerin <disconico@outlook.com>
2023-10-20 18:42:38 +0200

    change to postgres and add user

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,48 +1,53 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
-	"database/sql"
 	"flag"
-	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/pgxstore"
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx/v5/pgxpool"
 	"snippetbox.disconico/internal/models"
 )
 
 type application struct {
 	logger         *slog.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:nicolas78760@/snippetbox?parseTime=true", "MySQL data source name")
-	flag.Parse()
-
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}))
 
-	db, err := openDB(*dsn)
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		logger.Error("DSN not provided. Set the DB_DSN environment variable.")
+		os.Exit(1)
+	}
+
+	db, err := openDB(dsn)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
-	defer func(db *sql.DB) {
-		_ = db.Close()
-	}(db)
+	defer db.Close()
 
 	templateCache, err := newTemplateCache()
 	if err != nil {
@@ -53,13 +58,14 @@ func main() {
 	formDecoder := form.NewDecoder()
 
 	sessionManager := scs.New()
-	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Store = pgxstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
 	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		logger:         logger,
 		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		templateCache:  templateCache,
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
@@ -86,14 +92,19 @@ func main() {
 	os.Exit(1)
 }
 
-func openDB(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	poolConfig, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
 
 	if err != nil {
 		return nil, err
 	}
 
-	err = db.Ping()
+	err = db.Ping(context.Background())
 	if err != nil {
 		return nil, err
 	}
