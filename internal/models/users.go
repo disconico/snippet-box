754a5e7cd9126c1d961f6648d08f89bc3a90d75b
Nicolas Guerin <disconico@outlook.com>
2023-11-06 19:16:59 +0100

    add authenticate middleware.go

--- internal/models/users.go
+++ internal/models/users.go
@@ -73,5 +73,9 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = $1)"
+
+	err := m.DB.QueryRow(context.Background(), stmt, id).Scan(&exists)
+	return exists, err
 }
