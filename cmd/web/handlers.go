73fd768511c099ab97c7b72c1405a13a7b31c842
Nicolas Guerin <disconico@outlook.com>
2023-11-12 12:56:59 +0100

    add redirect after login

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -217,6 +217,12 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 
 	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
 
+	path := app.sessionManager.PopString(r.Context(), "redirectPathAfterLogin")
+	if path != "" {
+		http.Redirect(w, r, path, http.StatusSeeOther)
+		return
+	}
+
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -45,6 +45,7 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			if !app.isAuthenticated(r) {
+				app.sessionManager.Put(r.Context(), "redirectPathAfterLogin", r.URL.Path)
 				http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 				return
 			}
